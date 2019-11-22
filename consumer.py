a476ad96d446b0ca1a2d7e904618f97c0df7c28f
Khelil Sator <khelil.sator.git@gmail.com>
2019-11-22 19:18:39 +0100

    Create consumer.py

---
+++ consumer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+
+kafka = '100.123.35.0:9092'
+topic = "topic1" 
+
+# auto_offset_reset can be set to 'latest' or 'earliest'
+consumer1 = KafkaConsumer(topic, bootstrap_servers=kafka, security_protocol="PLAINTEXT", auto_offset_reset='earliest')
+
+# Return True if the client is connected to the kafka server 
+# consumer1.bootstrap_connected()
+
+# Get all topics the user is authorized to view. 
+# consumer1.topics()
+
+for message in consumer1:
+    print ("topic=%s offset=%d key=%s value=%s" % (message.topic, message.offset, message.key, message.value))

76a36221664460ab4db39f262909b5fb6905d2d4
Khelil Sator <khelil.sator.git@gmail.com>
2019-11-24 12:37:35 +0100

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,8 +2,6 @@ from kafka import KafkaProducer
 
 topic = "topic5" 
 kafka = '100.123.35.0:9092'
-messages = [{"key1":"value1"}, {"key2":"value2"}]
-
 messages = [{"key":"key1", "value": "value1"}, {"key":"key2", "value": "value2"}]
 
 producer = KafkaProducer(bootstrap_servers=[kafka], security_protocol="PLAINTEXT", retries=2, compression_type=None, request_timeout_ms=10000)

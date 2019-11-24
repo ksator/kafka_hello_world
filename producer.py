4403ffa43bb1716e198d59843f434b99a1a1c092
Khelil Sator <khelil.sator.git@gmail.com>
2019-11-24 12:37:07 +0100

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,14 +1,15 @@
 from kafka import KafkaProducer
 
-topic = "topic1" 
-key="key1"
-value="value1"
+topic = "topic5" 
 kafka = '100.123.35.0:9092'
+messages = [{"key1":"value1"}, {"key2":"value2"}]
+
+messages = [{"key":"key1", "value": "value1"}, {"key":"key2", "value": "value2"}]
 
 producer = KafkaProducer(bootstrap_servers=[kafka], security_protocol="PLAINTEXT", retries=2, compression_type=None, request_timeout_ms=10000)
 #producer.bootstrap_connected()
-message = producer.send(topic, key = key, value = value)
+
+for item in messages: 
+  message = producer.send(topic, key = item["key"], value = item["value"])
+
 producer.flush()
-
-
-
